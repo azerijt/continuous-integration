fb8b0231f0ed5af488ce3beb8e23eed4953cc987
Anna-Zeri Temir <azjuliantemir@gmail.com>
2022-02-23 14:54:27 +0000

    added jest test and changed from length to string

--- src/find-shortest-string.js
+++ src/find-shortest-string.js
@@ -14,9 +14,11 @@ function findShortestString(stringArr) {
       shortestString = str;
     }
   }
-  return shortestString.length;
+  return shortestString;
 }
 
 console.log(findShortestString(["it", "is", "a", "nice", "day"]), "a");
 console.log(findShortestString(["why", "hello", "to", "you"]), "to");
 console.log(findShortestString(["brave", "dance"]), "brave");
+
+module.exports = findShortestString;
